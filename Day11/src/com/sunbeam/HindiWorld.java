d683040c38ce0b20f33402c38f11297e97644195
Ritik <ritikwaghamare@gmail.com>
2024-09-15 23:25:58 +0530

    hello world

---
+++ Day11/src/com/sunbeam/HindiWorld.java
@@ -0,0 +1,13 @@
+package com.sunbeam;
+
+public class HindiWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("अंतः अस्ति प्रारंभः");
+		
+	}
+
+	
+}
